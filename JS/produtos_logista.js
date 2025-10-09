d7ae7ea9b05d9953c7e6a0ef374be9f3294e0b71
Gih-Web <giovanna.evellyn.web@gmail.com>
2025-10-09 17:49:24 -0300

    cadastro categoria

---
+++ JS/produtos_logista.js
@@ -0,0 +1,55 @@
+(async() => {
+    //
+    const sel  = document.querySelector("#pCategoria");
+  
+    
+
+    try {
+        //criando variável que guarda os dados vindo do php, que estão no metodo listar
+        const r  = await fetch("../PHP/cadastro_categorias.php?listar=1");
+        // Se o retorno do php vier falso significa que não foi possivel listar os dados 
+       if(!r.ok) throw new Error ("Falha ao listar categorias!");
+
+        /* SE vier dados do php, ele joga as informações dentro do campo html em formato de texto 
+        innerHTML - inserir dados em elementos html
+        */
+       
+        sel.innerHTML = await r.text ();
+     
+    } catch (e) { 
+
+        //se dê erro na listagem, aparece Erro ao carregar dentro do campo html 
+        sel.innerHTML = "<option disable> Erro ao carregar </option>";
+   
+          
+    }  
+  
+} )();      
+
+(async() => {
+    //
+
+    const sele  = document.querySelector("#prodCategoria");
+    
+
+    try {
+        //criando variável que guarda os dados vindo do php, que estão no metodo listar
+        const r  = await fetch("../PHP/cadastro_categorias.php?listar=1");
+        // Se o retorno do php vier falso significa que não foi possivel listar os dados 
+       if(!r.ok) throw new Error ("Falha ao listar categorias!");
+
+        /* SE vier dados do php, ele joga as informações dentro do campo html em formato de texto 
+        innerHTML - inserir dados em elementos html
+        */
+       
+
+         sele.innerHTML = await r.text ();
+    } catch (e) { 
+
+        //se dê erro na listagem, aparece Erro ao carregar dentro do campo html 
+
+        sele.innerHTML = "<option disable> Erro ao carregar </option>";
+          
+    }  
+  
+} )();      
